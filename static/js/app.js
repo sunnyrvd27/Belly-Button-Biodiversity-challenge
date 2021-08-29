942113b4c7b6a58fddc35a0f91c5701e2d40240f
sunnyrvd27 <sunnyrvd@gmail.com>
2021-08-28 17:39:58 -0700

    Fourth commit

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,9 @@
 // Read the data file
 
-url = "https://raw.githubusercontent.com/sunnyrvd27/Belly-Button-Biodiversity-challenge/tree/main/data/samples.json"
-url2 = "https://github.com/sunnyrvd27/Belly-Button-Biodiversity-challenge/blob/main/data/samples.json"
-file = "samples.json"
+//url = "https://raw.githubusercontent.com/sunnyrvd27/Belly-Button-Biodiversity-challenge/tree/main/data/samples.json"
+//url2 = "https://github.com/sunnyrvd27/Belly-Button-Biodiversity-challenge/blob/main/data/samples.json"
+file1 = "samples.json"
 
-d3.json(file, function(data) {
+d3.json(file1).then(function(data) {
     console.log(data);
-  });
+});
